e08dcd6cf5073fa5dbef87f8a62619163f9ed0bc
korgon <korgon@gmail.com>
2015-12-03 22:23:43 -0700

    Progress towards rework...

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// begin
+"use strict";
+
 var conf = require('_/config')(__dirname);
 var logit = require('_/logit');
 var springboard = require('_/springboard');
 
-springboard.init(conf).catch(function(err) {
-  console.log('failed to initialize springboard!');
+springboard.init(conf).then(function() {
+  var app = require('_/app')(springboard);
+  console.log('success!');
+}).catch(function(err) {
+  console.log('failed to initialize the springboard!');
+  console.log(err);
   process.exit(1);
 });
-
-var app = require('_/app')(springboard);
-
-if (conf.app_log_http) {
-  app.use(logger());
-}
-
-app.use(serve(global.site_repo_dir));
-
-// start your engines
-app.listen(conf.port + 1);
