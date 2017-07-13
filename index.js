3b2f6f6dbc9629984e5bb590cb9b06308c297b89
kevin <khogg@kevins-iMac.local>
2017-07-13 14:18:55 -0600

    Updated docs and executables.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// note that "_" is a clever way for including modules without typing full path
+// "_" is a symbolic link inside of the root node_modules folder
+// _ -> ../lib
+
 var conf = require('_/config')(__dirname);
 var springboard = require('_/core');
 
